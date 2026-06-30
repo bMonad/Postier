9c70fc573c7ca06cdb801dd5fca68b6a9afaf093
Shiva1706 <shivansh.singh20021@gmail.com>
2026-06-30 11:51:55 +0530

    Add server project files to parent repository

---
+++ Postier_Server/Persistence/PostierDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence;
+
+public class PostierDbContext(DbContextOptions options) :DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
---
+++ Postier_Server/Postier_API/Controllers/ActivitiesController.cs
@@ -0,0 +1,39 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Postier_API.Controllers;
+
+public class ActivitiesController(PostierDbContext context) : ActivitiesBaseController
+{
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityById(string id)
+    {
+        try
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var response = await context.Activities.FindAsync(id);
+            if (response == null)
+            {
+                return NotFound();
+            }
+            return Ok(response);
+        }
+        catch (Exception ex) 
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while fetching the activity. "+"\n"+ ex);
+        }
+    }
+}
